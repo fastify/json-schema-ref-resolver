fd7d76398d02409bb8d159b07c1bbaca541cdb7c
Ivan Tymoshenko <ivan@tymoshenko.me>
2023-10-13 20:34:14 +0200

    feat: add json schema ref resolver

---
+++ types/index.test-d.ts
@@ -0,0 +1,21 @@
+import { RefResolver } from '..'
+import { expectType } from 'tsd'
+
+const resolver = new RefResolver({
+  allowEqualDuplicates: true
+})
+
+expectType<void>(resolver.addSchema({}))
+expectType<void>(resolver.addSchema({}, 'schemaId'))
+
+expectType<any | null>(resolver.getSchema('schemaId'))
+expectType<any | null>(resolver.getSchema('schemaId', 'jsonPointer'))
+
+expectType<{ schemaId: string; jsonPointer: string }[]>(resolver.getSchemaRefs('schemaId'))
+
+expectType<{ [key: string]: any }>(resolver.getSchemaDependencies('schemaId'))
+
+expectType<void>(resolver.derefSchema('schemaId'))
+
+expectType<any | null>(resolver.getDerefSchema('schemaId'))
+expectType<any | null>(resolver.getDerefSchema('schemaId', 'jsonPointer'))
