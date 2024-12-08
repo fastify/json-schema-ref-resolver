1da0bd9bac005fd33c7c15fa47146e8ecff2cd65
Frazer Smith <frazer.dev@icloud.com>
2024-12-08 10:45:33 +0000

    build(deps-dev): replace standard with neostandard

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,19 +4,18 @@
   * @constructor
   */
 declare class RefResolver {
-
   /**
    * @param {object} opts - Options for the resolver.
    * @param {boolean} opts.allowEqualDuplicates - Whether to allow schemas with equal ids to be added to the resolver.
    */
-  constructor(opts?: { allowEqualDuplicates?: boolean });
+  constructor (opts?: { allowEqualDuplicates?: boolean })
 
   /**
    * Adds the given schema to the resolver.
    * @param {any} schema - The schema to be added.
    * @param {string} schemaId - The default schema id of the schema to be added.
    */
-  addSchema(schema: any, schemaId?: string): void;
+  addSchema (schema: any, schemaId?: string): void
 
   /**
    * Returns the schema by the given schema id and jsonPointer.
@@ -25,34 +24,34 @@ declare class RefResolver {
    * @param {string} jsonPointer - The jsonPointer of the schema to be returned.
    * @returns {any | null} The schema by the given schema id and jsonPointer.
    */
-  getSchema(schemaId: string, jsonPointer?: string): any | null;
+  getSchema (schemaId: string, jsonPointer?: string): any | null
 
   /**
    * Returns true if the schema by the given schema id is added to the resolver.
    * @param {string} schemaId - The schema id of the schema to be checked.
    * @returns {boolean} True if the schema by the given schema id is added to the resolver.
    */
-  hasSchema(schemaId: string): boolean;
+  hasSchema (schemaId: string): boolean
 
   /**
    * Returns the schema references of the schema by the given schema id.
    * @param {string} schemaId - The schema id of the schema whose references are to be returned.
    * @returns {Array<{ schemaId: string; jsonPointer: string }>} The schema references of the schema by the given schema id.
    */
-  getSchemaRefs(schemaId: string): { schemaId: string; jsonPointer: string }[];
+  getSchemaRefs (schemaId: string): { schemaId: string; jsonPointer: string }[]
 
   /**
    * Returns all the schema dependencies of the schema by the given schema id.
    * @param {string} schemaId - The schema id of the schema whose dependencies are to be returned.
    * @returns {object} The schema dependencies of the schema by the given schema id.
    */
-  getSchemaDependencies(schemaId: string): { [key: string]: any };
+  getSchemaDependencies (schemaId: string): { [key: string]: any }
 
   /**
    * Dereferences the schema by the given schema id.
    * @param {string} schemaId - The schema id of the schema to be dereferenced.
    */
-  derefSchema(schemaId: string): void;
+  derefSchema (schemaId: string): void
 
   /**
    * Returns the dereferenced schema by the given schema id and jsonPointer.
@@ -62,7 +61,7 @@ declare class RefResolver {
    * @param {string} jsonPointer - The jsonPointer of the schema to be returned.
    * @returns {any | null} The dereferenced schema by the given schema id and jsonPointer.
    */
-  getDerefSchema(schemaId: string, jsonPointer?: string): any | null;
+  getDerefSchema (schemaId: string, jsonPointer?: string): any | null
 }
 
-export { RefResolver };
+export { RefResolver }
